c5082b39fde3a6806f74ce93de0f55a31f790fef
rontech <hy-zhang@rontech.co.jp>
2015-06-19 13:58:52 +0900

    Edit sbt

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,33 @@
 package controllers;
 
+import java.util.List;
+import controllers.Forms;
+
+import controllers.routes;
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
+
+import play.data.Form;
+import models.*;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import play.libs.Json;
 
 public class Application extends Controller {
 
+    static Form<Forms.StandForm> standForm = Form.form(Forms.StandForm.class);
+    /** indexへのレンダリング */
     public Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render(Stand.all(),standForm));
     }
 
+    public Result addStand() {
+	    Form<Forms.StandForm>filledForm = standForm.bindFromRequest();
+	    /**変数でフォームに入力した内容を返す*/
+        JsonNode getInput = Stand.create(filledForm.get());
+
+        return ok(seclet.render(getInput));
+    }
 }
